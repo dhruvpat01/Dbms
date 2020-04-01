d15b2b1338eb75fbcadb7aa4ccf8cb23f10230e3
daksh1000 <62137232+daksh1000@users.noreply.github.com>
2020-04-01 15:00:41 +0530

    Add files via upload

--- Delete_Record.py
+++ Delete_Record.py
@@ -1,6 +1,9 @@
 from tkinter import *
-from sqlite3 import *
+import sqlite3
 from Fullscrn import *
+from Admin import *
+from tkinter import messagebox
+import re
 
 
 
@@ -11,10 +14,10 @@ def delet(d):
     Frame1.place(relx=0.02, rely=0.02, relheight=0.94, relwidth=0.96)
     Frame1.configure(borderwidth="2", background="#BD081C", width=500)
 
-    delete1 = Button(Frame1, background="#d9d9d9", text="Delete User Records", fg='black', font=("Courier", 25), width=200, command=lambda :byemail(obj4))
-    delete2 = Button(Frame1, background="#d9d9d9", text="Delete Donor Records", fg='black', font=("Courier", 25), width=300, command=lambda : byemail(obj4))
-    delete3 = Button(Frame1, background="#d9d9d9", text="Delete Receiver Records", fg='black', font=("Courier", 25), width=200, command=lambda : byemail(obj4))
-    delete4 = Button(Frame1, background="#d9d9d9", text="Delete Blood Inventory", fg='black', font=("Courier", 25), width=200, command=lambda : byblood(obj4))
+    delete1 = Button(Frame1, background="#d9d9d9", text="Delete User Records", fg='black', font=("Courier", 25), width=200,command=lambda :byemail(obj4))
+    delete2 = Button(Frame1, background="#d9d9d9", text="Delete Donor Records", fg='black', font=("Courier", 25), width=300,command=lambda :gyemail(obj4))
+    delete3 = Button(Frame1, background="#d9d9d9", text="Delete Receiver Records", fg='black', font=("Courier", 25), width=200,command=lambda :kyemail(obj4))
+    delete4 = Button(Frame1, background="#d9d9d9", text="Delete Blood Inventory", fg='black', font=("Courier", 25), width=200,command=lambda :byblood(obj4))
 
     delete1.place(relx=0.35, rely=0.25, height=53, width=400)
     delete2.place(relx=0.35, rely=0.35, height=53, width=400)
@@ -39,12 +42,102 @@ def byemail(a):
     email2 = Entry(d)
     email2.place(relx=0.47, rely=0.45, height=33, width=200)
 
-    delete1 = Button(Frame1, background="#d9d9d9", text="Delete", fg='black', font=("Times", 25), width=200)
+    delete1 = Button(Frame1, background="#d9d9d9", text="Delete", fg='black', font=("Times", 25), width=200,command=lambda :UserDel(email2.get(),d))
     delete1.place(relx=0.47, rely=0.57, height=43, width=200)
     delete1.configure()
 
     full11=FullScreenApp(d)
     d.mainloop()
+
+
+def UserDel(email,d):
+    conn = sqlite3.connect("Blood_Bank.db")
+    c = conn.cursor()
+    c.execute("Select * FROM User WHERE Email=(?)",(email,))
+    rec=c.fetchone()
+    if rec is None:
+        y=messagebox.showwarning("popup","No such record exists")
+    else:
+        c.execute("DELETE FROM User WHERE Email=(?)", (email,))
+        l = messagebox.showwarning("popup", "Deleted")
+        d.destroy()
+        conn.commit()
+
+
+def gyemail(a):
+    a.destroy()
+    d = Tk()
+    d.title("Delete page")
+
+    Frame1 = Frame(d)
+    Frame1.place(relx=0.02, rely=0.02, relheight=0.94, relwidth=0.96)
+    Frame1.configure(borderwidth="2", background="#BD081C", width=500)
+
+    email1 = Label(Frame1)
+    email1.place(relx=0.30, rely=0.45, height=33, width=200)
+    email1.configure(background="#d9d9d9", text="Enter Email:", font=("Times", 10), width=1000)
+    email2 = Entry(d)
+    email2.place(relx=0.47, rely=0.45, height=33, width=200)
+
+    delete1 = Button(Frame1, background="#d9d9d9", text="Delete", fg='black', font=("Times", 25), width=200,command=lambda :gserDel(email2.get(),d))
+    delete1.place(relx=0.47, rely=0.57, height=43, width=200)
+    delete1.configure()
+
+    full11=FullScreenApp(d)
+    d.mainloop()
+
+
+def gserDel(email,d):
+    conn = sqlite3.connect("Blood_Bank.db")
+    c = conn.cursor()
+    c.execute("Select * FROM Receiver WHERE Email=(?)",(email,))
+    rec=c.fetchone()
+    if rec is None:
+        y=messagebox.showwarning("popup","No such record exists")
+    else:
+        c.execute("DELETE FROM Receiver WHERE Email=(?)", (email,))
+        l = messagebox.showwarning("popup", "Deleted")
+        d.destroy()
+        conn.commit()
+
+
+def kyemail(a):
+    a.destroy()
+    d = Tk()
+    d.title("Delete page")
+
+    Frame1 = Frame(d)
+    Frame1.place(relx=0.02, rely=0.02, relheight=0.94, relwidth=0.96)
+    Frame1.configure(borderwidth="2", background="#BD081C", width=500)
+
+    email1 = Label(Frame1)
+    email1.place(relx=0.30, rely=0.45, height=33, width=200)
+    email1.configure(background="#d9d9d9", text="Enter Email:", font=("Times", 10), width=1000)
+    email2 = Entry(d)
+    email2.place(relx=0.47, rely=0.45, height=33, width=200)
+
+    delete1 = Button(Frame1, background="#d9d9d9", text="Delete", fg='black', font=("Times", 25), width=200,command=lambda :kserDel(email2.get(),d))
+    delete1.place(relx=0.47, rely=0.57, height=43, width=200)
+    delete1.configure()
+
+    full11=FullScreenApp(d)
+    d.mainloop()
+
+
+def kserDel(email,d):
+    conn = sqlite3.connect("Blood_Bank.db")
+    c = conn.cursor()
+    c.execute("Select * FROM Donor WHERE Email=(?)",(email,))
+    rec=c.fetchone()
+    if rec is None:
+        y=messagebox.showwarning("popup","No such record exists")
+    else:
+        c.execute("DELETE FROM Donor WHERE Email=(?)", (email,))
+        l = messagebox.showwarning("popup", "Deleted")
+        d.destroy()
+        conn.commit()
+
+
 
 def byblood(a):
     a.destroy
@@ -55,15 +148,39 @@ def byblood(a):
     Frame1.place(relx=0.02, rely=0.02, relheight=0.94, relwidth=0.96)
     Frame1.configure(borderwidth="2", background="#BD081C", width=500)
 
-    email1 = Label(Frame1)
-    email1.place(relx=0.30, rely=0.45, height=33, width=200)
-    email1.configure(background="#d9d9d9", text="Enter Blood Group:", font=("Times", 10), width=1000)
-    email2 = Entry(e)
-    email2.place(relx=0.47, rely=0.45, height=33, width=200)
+    b = StringVar(Frame1)
+    pblood = Label(e, text="Select Your Blood Group ")
+    pblood.configure(background="#d9d9d9", font=("Times", 10))
+    pblood.place(relx=0.30, rely=0.37, height=33, width=200)
+    values = ["A Positive",
+              'A Negative',
+              'B Positive',
+              'B Negative',
+              'AB Positive',
+              'AB Negative',
+              'O Positive',
+              'O Negative']
+    b.set('Select Your Blood Type')
+    dropdown = OptionMenu(e, b, *values)
+    dropdown.place(relx=0.47, rely=0.37, height=33, width=200)
 
-    delete1 = Button(Frame1, background="#d9d9d9", text="Delete", fg='black', font=("Times", 25), width=200)
+    delete1 = Button(Frame1, background="#d9d9d9", text="Delete", fg='black', font=("Times", 25), width=200,command=lambda :Bloodel(b.get(),e))
     delete1.place(relx=0.47, rely=0.57, height=43, width=200)
     delete1.configure()
 
     full11 = FullScreenApp(e)
     e.mainloop()
+
+
+def Bloodel(blood,e):
+    conn = sqlite3.connect("Blood_Bank.db")
+    c = conn.cursor()
+    c.execute("Select * FROM Blood_Inventory WHERE Blood_Group=(?)",(blood,))
+    rec=c.fetchone()
+    if rec is None:
+        y=messagebox.showwarning("popup","No such record exists")
+    else:
+        c.execute("DELETE FROM Blood_Inventory WHERE Blood_Group=(?)", (blood,))
+        l = messagebox.showwarning("popup", "Deleted")
+        e.destroy()
+        conn.commit()
